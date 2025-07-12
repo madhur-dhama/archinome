0ef3da1f334ef76c9f799332bbe5f9e823da53ea
Madhur Dhama <137516469+madhur-dhama@users.noreply.github.com>
2025-07-12 20:55:17 +0530

    Update gnome-settings.sh

--- gnome/gnome-settings.sh
+++ gnome/gnome-settings.sh
@@ -3,5 +3,5 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark"
 echo "setting wallpaper to ${SCRIPT_DIR}/wallpaper.jpeg"
-gsettings set org.gnome.desktop.background picture-uri-dark "file://${SCRIPT_DIR}/wallpaper.jpeg"
+gsettings set org.gnome.desktop.background picture-uri-dark "file://${SCRIPT_DIR}/wallpaper.jpg"
 gsettings set org.gnome.desktop.background picture-options 'zoom'
