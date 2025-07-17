44e8eca541a0ced2c48b7dfed71d615a44cd0912
Madhur Dhama <137516469+madhur-dhama@users.noreply.github.com>
2025-07-17 18:58:59 +0530

    Update gnome-settings.sh

--- gnome/gnome-settings.sh
+++ gnome/gnome-settings.sh
@@ -4,12 +4,12 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
 gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark"
 # Seting wallpaper 
-#echo "setting wallpaper to ${SCRIPT_DIR}/wallpaper.jpeg"
-#gsettings set org.gnome.desktop.background picture-uri-dark "file://${SCRIPT_DIR}/wallpaper.jpg"
-#gsettings set org.gnome.desktop.background picture-options 'zoom'
+echo "setting wallpaper to ${SCRIPT_DIR}/wallpaper.jpeg"
+gsettings set org.gnome.desktop.background picture-uri-dark "file://${SCRIPT_DIR}/wallpaper2.png"
+gsettings set org.gnome.desktop.background picture-options 'zoom'
 
 # Diaplay scale to 125%
-#gsettings set org.gnome.desktop.interface text-scaling-factor 1.25
+gsettings set org.gnome.desktop.interface text-scaling-factor 1.25
 
 # Enable ulauncher at startup 
 systemctl --user enable --now ulauncher
